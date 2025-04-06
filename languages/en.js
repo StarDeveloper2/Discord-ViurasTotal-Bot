9863083309b89b2682e11519fca9a57b781a1524
Star Developmen <inawaf781@gmail.com>
2025-04-06 04:22:44 +0300

    Add files via upload

---
+++ languages/en.js
@@ -0,0 +1,24 @@
+module.exports = {
+    scanLinkService: '🔍 Link Scanning Service',
+    scanLinkDescription: 'Scan any link by pressing the button below to ensure its safety using VirusTotal.\n\n📌 Make sure the link starts with `http://` or `https://`',
+    scanButtonLabel: '🔎 Scan Link',
+    enterUrl: 'Enter the URL to scan :',
+    linkSafeMessage: '✅ The link is safe',
+    linkMaliciousMessage: '🚨 The link contains threats',
+    errorOccurred: '❌ An error occurred while scanning the link.',
+    logTitleSafe: '✅ Link Scan Log (Safe)',
+    logTitleMalicious: '📛 Link Scan Log (Malicious)',
+    scanResultMalicious: 'Detected by {0} engines',
+    scanResultSafe: 'The link is completely safe',
+    footer: 'VirusTotal Scanner',
+    setlanguage: 'Language has been successfully changed to:',
+
+    // Log Labels
+    userLabel: '👤 User :',
+    resultLabel: '📂 URL Result :',
+    urlLabel: '🔗 URL :',
+
+    // Viurastotal commands [LANG]
+    viurastotalDescription: 'Scan a link using VirusTotal',
+    urlOptionDescription: 'Enter the link to be scanned'
+  };
