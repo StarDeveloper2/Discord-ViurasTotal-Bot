aaba1d70a04a1e1a4b0e14f1c1b30e4cd54d4203
Star Developmen <inawaf781@gmail.com>
2025-04-06 04:18:22 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    token: 'YOUR_BOT_TOKEN', // This is the bot's token. You get it from the Discord Developer Portal.
+    virustotalApiKey: 'YOUR_VIRUSTOTAL_API_KEY', // This is the API key you get from VirusTotal after creating an account.
+    language: 'YOUR_BOT_LANGUAGE', // Set the bot's language. Choose 'en' for English or 'ar' for Arabic.
+    
+    clientId: 'YOUR_CLIENT_ID', // This is your Discord application's client ID, found in the Discord Developer Portal.
+    guildId: 'YOUR_GUILD_ID', // This is the Discord server (guild) ID where the bot is used.
+
+    scanChannelId: 'YOUR_SCAN_CHANNEL_ID', // Channel ID for sending link scan results when button is clicked.
+    logChannelId: 'YOUR_LOG_CHANNEL_ID' // This is the ID of the channel where the bot will log details of the scans.
+};
+  
